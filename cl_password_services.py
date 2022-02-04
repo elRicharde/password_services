ea96b11886d651a9b5e1f262c2ecbcad0f03325a
elRicharde <52285099+elRicharde@users.noreply.github.com>
2022-02-04 20:13:33 +0100

    renaming

--- cl_password_services.py
+++ cl_password_services.py
@@ -7,6 +7,7 @@ import getpass
 import time
 from os import system
 
+
 # https://stackoverflow.com/questions/62514742/how-to-store-hashed-passwords-on-sqlite3-database
 
 
@@ -23,25 +24,25 @@ class PasswordService:
 
     def __check_password(self, hashed_password, user_password):
         password, salt = hashed_password.split(':')
-        return password == hashlib.sha256(salt.encode() + user_password.encode("utf-8")).hexdigest()      # True / False
+        return password == hashlib.sha256(salt.encode() + user_password.encode("utf-8")).hexdigest()  # True / False
 
     def get_new_password(self):
         first = "first"
         second = "kontrolle"
         msg = "nix"
-        
+
         print("Passwort eingeben - Achtung, aus Sicherheitsgründen gibt es kein Cursor")
-                
+
         while first != second:
-            system('cls')       # clear screen
-            
+            system('cls')  # clear screen
+
             if msg != "nix":
                 print(msg)
             first = getpass.getpass(prompt="Password: ", stream=None)
             second = getpass.getpass(prompt="Password wiederholen: ", stream=None)
             msg = "Eingabe nicht identisch, bitte neu Beginnen"
-        
-        return self.__hash_password(first)    # return hashed pw
+
+        return self.__hash_password(first)  # return hashed pw
 
     def authorize(self, hashed_password):
         wait = 2
@@ -51,14 +52,14 @@ class PasswordService:
             while self.__check_password(hashed_password, getpass.getpass(prompt="Password: ", stream=None)) is False:
                 wait = wait * 2
                 for sec in range(wait, 0, -1):
-                    system('cls')       # clear screen
+                    system('cls')  # clear screen
                     print("Passwort falsch, neuer Versuch in ", str(sec), " Sekunden")
                     time.sleep(1)
-                system('cls')       # clear screen
+                system('cls')  # clear screen
                 print("Anmelden abbrechen mit 'Ctrl + C'")
                 print("aus Sicherheitsgründen gibt es kein Cursor")
-                    
+
         except KeyboardInterrupt:
-            return False    # nicht autorisiert
+            return False  # nicht autorisiert
 
         return True  # autorisiert
